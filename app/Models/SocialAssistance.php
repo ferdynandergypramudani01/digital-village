26ca21ab7ed70a5559b6fa13981021b663f945a7
f3rc0n5 <ferdynandergypramudani15.com>
2026-08-19 14:40:41 +0700

    feat: add SocialAssistance Model and Migration

---
+++ app/Models/SocialAssistance.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UUID;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SocialAssistance extends Model
+{
+    use UUID, SoftDeletes;
+
+    protected $fillable = [
+        'thumbnail',
+        'name',
+        'category',
+        'amount',
+        'provider',
+        'description',
+        'is_available'
+
+    ];
+
+    
+    
+}
