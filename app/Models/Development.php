675d7af58aa1f98e5a6a00ac7b066e88a8f9aff6
f3rc0n5 <ferdynandergypramudani15.com>
2026-08-19 16:51:05 +0700

    feat: add Development Model and Migration

---
+++ app/Models/Development.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UUID;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Development extends Model
+{
+    use UUID, SoftDeletes;
+
+    protected $fillable = [
+        'thumbnail',
+        'name',
+        'description',
+        'person_in_charge',
+        'start_date',
+        'end_date',
+        'amount',
+        'status'
+    ];
+}
