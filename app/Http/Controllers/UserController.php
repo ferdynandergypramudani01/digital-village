7a52cf0ef8df10300ca6985c66f0e9424188b06d
f3rc0n5 <ferdynandergypramudani01@gmail.com>
2026-08-23 13:26:02 +0700

    feat: add Destroy User Data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -123,6 +123,21 @@ class UserController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $user = $this->userRepository->getById($id);
+
+            if (!$user) {
+                return ResponseHelper::jsonResponse(false, 'User tidak ditemukan', null, 404);
+
+            }
+
+            $user = $this->userRepository->delete(
+                $id
+            );
+
+            return ResponseHelper::jsonResponse(true, 'Data User Berhasil Dihapus', new UserResource($user), 200);
+        } catch (\Exception $e) {
+            return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
+        }
     }
 }
