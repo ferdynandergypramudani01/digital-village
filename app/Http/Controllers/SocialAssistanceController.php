2e7625310a22e3958336576fc43c9d170c6d42ed
f3rc0n5 <ferdynandergypramudani01@gmail.com>
2026-09-02 15:15:09 +0700

    feat: add GetAll and GetAllPagination

---
+++ app/Http/Controllers/SocialAssistanceController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\ResponseHelper;
+use App\Http\Resources\PaginateResource;
+use App\Http\Resources\SocialAssistanceResource;
+use App\Interfaces\SocialAssistanceRepositoryInterface;
+use Illuminate\Http\Request;
+
+class SocialAssistanceController extends Controller
+{
+    private SocialAssistanceRepositoryInterface $socialAssistanceRepository;
+
+    public function __construct(SocialAssistanceRepositoryInterface $socialAssistanceRepository)
+    {
+        $this->socialAssistanceRepository = $socialAssistanceRepository;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        try {
+            $socialAssistances = $this->socialAssistanceRepository->getAll(
+                $request->input('search'),
+                $request->limit,
+                true
+            );
+
+            return ResponseHelper::jsonResponse(true, 'Data Bantuan Sosial Berhasil Diambil', SocialAssistanceResource::collection($socialAssistances), 200);
+        } catch (\Exception $e) {
+            return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
+        }
+    }
+
+    
+    public function getAllPaginated(Request $request)
+    {
+        $request = $request->validate([
+            'search' => 'nullable|string',
+            'row_per_page' => 'required|integer',
+        ]);
+
+        try {
+            $socialAssistances = $this->socialAssistanceRepository->getAllPaginated(
+                $request['search'] ?? null,
+                $request['row_per_page'],
+            );
+
+            return ResponseHelper::jsonResponse(true, 'Data Bantuan Sosial Berhasil Diambil', PaginateResource::make($socialAssistances, SocialAssistanceResource::class), 200);
+        } catch (\Exception $e) {
+            return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
+        }
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- app/Models/SocialAssistance.php
+++ app/Models/SocialAssistance.php
@@ -19,7 +19,15 @@ class SocialAssistance extends Model
         'description',
         'is_available'
 
-    ];    
+    ];   
+
+    
+    public function scopeSearch($query, $search)
+    {
+        return $query->where('name', 'like', '%' . $search . '%')
+            ->orWhere('provider', 'like', '%' . $search . '%')
+            ->orWhere('amount', 'like', '%' . $search . '%');
+    }
 
     public function socialAssistanceRecipients()
     {
