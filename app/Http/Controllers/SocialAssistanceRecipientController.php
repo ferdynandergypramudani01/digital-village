ac15a82c2677ef5aa037df4628b5b2d845453786
f3rc0n5 <ferdynandergypramudani01@gmail.com>
2026-09-07 09:58:27 +0700

    feat: add Create Data for SocialAssistanceRecipient

--- app/Http/Controllers/SocialAssistanceRecipientController.php
+++ app/Http/Controllers/SocialAssistanceRecipientController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\ResponseHelper;
+use App\Http\Requests\SocialAssistanceRecipientStoreRequest;
 use App\Http\Resources\PaginateResource;
 use App\Http\Resources\SocialAssistanceRecipientResource;
 use App\Interfaces\SocialAssistanceRecipientRepositoryInterface;
@@ -56,9 +57,17 @@ class SocialAssistanceRecipientController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(SocialAssistanceRecipientStoreRequest $request)
     {
-        //
+        $request = $request->validated();
+
+        try {
+            $socialAssistanceRecipient = $this->socialAssistanceRecipientRepository->create($request);
+
+            return ResponseHelper::jsonResponse(true, 'Data Penerima Bantuan Sosial Berhasil Dibuat', new SocialAssistanceRecipientResource($socialAssistanceRecipient), 201);
+        } catch (\Exception $e) {
+            return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
+        }
     }
 
     /**
---
+++ app/Http/Requests/SocialAssistanceRecipientStoreRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class SocialAssistanceRecipientStoreRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'social_assistance_id' => 'required|exists:social_assistances,id',
+            'head_of_family_id' => 'required|exists:head_of_families,id',
+            'amount' => 'required|numeric|min:0',
+            'reason' => 'required|string',
+            'bank' => 'required|string|in:bri,bni,bca,mandiri',
+            'account_number' => 'required',
+            'proof' => 'nullable|image',
+            'status' => 'nullable|string|in:pending,approved,rejected'
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'social_assistance_id' => 'Bantuan Sosial',
+            'head_of_family_id' => 'Kepala Keluarga',
+            'amount' => 'Nominal',
+            'reason' => 'Alasan',
+            'bank' => 'Bank',
+            'account_number' => 'Nomor Rekening',
+            'proof' => 'Bukti Penerimaan',
+            'status' => 'Status Pengajuan'
+        ];
+    }
+}
