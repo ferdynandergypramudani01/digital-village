939802f339da5584e87e08069f1df9db2f81cc4d
f3rc0n5 <ferdynandergypramudani01@gmail.com>
2026-09-10 15:01:44 +0700

    feat: Add Update Data for Event

--- app/Repositories/EventRepository.php
+++ app/Repositories/EventRepository.php
@@ -52,7 +52,7 @@ class EventRepository implements EventRepositoryInterface
         string $id
     ) {
         $query = Event::where('id', $id);
-        
+
         return $query->first();
     }
 
@@ -60,6 +60,8 @@ class EventRepository implements EventRepositoryInterface
     public function create(
         array $data
     ) {
+        DB::beginTransaction();
+
         try {
             $event = new Event;
             $event->thumbnail = $data['thumbnail']->store('assets/events', 'public');
@@ -68,7 +70,42 @@ class EventRepository implements EventRepositoryInterface
             $event->price = $data['price'];
             $event->date = $data['date'];
             $event->time = $data['time'];
-            
+
+            if (isset($data['is_active'])) {
+                $event->is_active = $data['is_active'];
+            }
+
+            $event->save();
+
+            DB::commit();
+            return $event;
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            throw new Exception($e->getMessage());
+        }
+    }
+
+
+    public function update(
+        string $id,
+        array $data
+    ) {
+        DB::beginTransaction();
+
+        try {
+            $event = Event::find($id);
+
+            if(isset($data['thumbnail'])) {
+                $event->thumbnail = $data['thumbnail']->store('assets/events', 'public');
+            }
+
+            $event->name = $data['name'];
+            $event->description = $data['description'];
+            $event->price = $data['price'];
+            $event->date = $data['date'];
+            $event->time = $data['time'];
+
             if (isset($data['is_active'])) {
                 $event->is_active = $data['is_active'];
             }
