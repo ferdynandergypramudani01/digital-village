2503c419706d54dc0456311aadecf726a4890f0a
f3rc0n5 <ferdynandergypramudani01@gmail.com>
2026-09-12 16:48:08 +0700

    feat: Add Delete Data ById for EventParticipant

--- app/Repositories/EventParticipantRepository.php
+++ app/Repositories/EventParticipantRepository.php
@@ -115,4 +115,25 @@ class EventParticipantRepository implements EventParticipantRepositoryInterface
             throw new Exception($e->getMessage());
         }       
     }
+
+
+
+    public function delete(
+        string $id
+    ) {
+        DB::beginTransaction();
+
+        try {
+            $eventParticipant = EventParticipant::find($id);
+            $eventParticipant->delete();
+
+            DB::commit();
+
+            return $eventParticipant;
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            throw new Exception($e->getMessage());
+        }
+    }
 }
