6d5beb900663d9ff57f8cb12ecc2526b03988f2f
f3rc0n5 <ferdynandergypramudani01@gmail.com>
2026-09-14 15:45:00 +0700

    feat: Add Create Data for DevelopmentApplicant

--- app/Http/Controllers/DevelopmentApplicantController.php
+++ app/Http/Controllers/DevelopmentApplicantController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\ResponseHelper;
+use App\Http\Requests\DevelopmentApplicantStoreRequest;
 use App\Http\Resources\DevelopmentApplicantResource;
 use App\Http\Resources\PaginateResource;
 use App\Interfaces\DevelopmentApplicantRepositoryInterface;
@@ -58,9 +59,17 @@ class DevelopmentApplicantController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(DevelopmentApplicantStoreRequest $request)
     {
-        //
+        $request = $request->validated();
+
+        try {
+            $developmentApplicant = $this->developmentApplicantRepository->create($request);
+
+            return ResponseHelper::jsonResponse(true, 'Data Pendaftar Pembangunan Berhasil Dibuat', new DevelopmentApplicantResource($developmentApplicant), 201);
+        } catch (\Exception $e) {
+            return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
+        }
     }
 
     /**
