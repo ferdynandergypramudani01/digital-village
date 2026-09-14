974032df7a67399d077a0286d2d4ddc55d37f6b7
f3rc0n5 <ferdynandergypramudani01@gmail.com>
2026-09-14 12:17:17 +0700

    feat: Add Delete Data for Development

--- app/Http/Controllers/DevelopmentController.php
+++ app/Http/Controllers/DevelopmentController.php
@@ -120,6 +120,18 @@ class DevelopmentController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $development = $this->developmentRepository->getById($id);
+
+            if(!$development) {
+                return ResponseHelper::jsonResponse(true, 'Data Pembangunan Tidak Ditemukan', null, 404);
+            }
+
+            $development = $this->developmentRepository->delete($id);
+
+            return ResponseHelper::jsonResponse(true, 'Data Pembangunan Berhasil di Hapus', null, 200);
+        } catch (\Exception $e) {
+            return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
+        }
     }
 }
